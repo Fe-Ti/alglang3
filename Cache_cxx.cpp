9bb007cd5471a94caa8651721b724b484c3b0a6a
Fёdor T <btm.007@mail.ru>
2021-10-16 19:27:36 +0300

    Fixed some stuff

--- Cache_cxx.cpp
+++ Cache_cxx.cpp
@@ -19,8 +19,8 @@ get_fwd_average_latency(const size_t data_size,
      */
     size_t average_latency = 0;
 
-    int n;
-    int* data = new int[data_size];
+    char n;
+    char* data = new char[data_size];
     for(size_t j = 0; j < data_size; j += streak_size)
         data[j] = rand();
 
@@ -30,18 +30,18 @@ get_fwd_average_latency(const size_t data_size,
             n = data[j];
     }
     auto end_t = std::chrono::steady_clock::now();
-    delete [] data;
-    average_latency
-      += std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t)
-           .count()
-         / iterations;
+    delete[] data;
+    average_latency +=
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t)
+        .count() /
+      iterations;
     return average_latency;
 }
 
 void
 output_percentage(const short int score)
 {
-    std::cout << "[1A[" << std::setw(3) << score << "% ]" << std::endl;
+    std::cout << "\033[1A[" << std::setw(3) << score << "% ]" << std::endl;
 }
 //
 // uint8_t
@@ -86,115 +86,100 @@ int
 main()
 {
     std::string str;
-    float scale = 2.0;
-    
+
     size_t tzero = 10;
     size_t tnext = 0;
+    size_t tdelta = 0;
     char n = '\0';
+    size_t iterations = 1000;
     size_t ss_end_val = 0;
-    std::cout << "Should I look at streak size? (0 to skip)" << std::endl;
-    std::cin >> ss_end_val;
-    size_t iterations = 1000000;//000;
-    for(size_t ss = 0; ss < ss_end_val; ++ss){
-        tzero = 0;
-        tnext = 0;
-        std::cout << "[     ] Checking " << ss << std::endl;
-        for(size_t i = 0; i < iterations; ++i) {
-            char* data = new char[ss];
-
-            n = data[0];
-            auto start_t = std::chrono::steady_clock::now();
-            n = data[0];
-            auto end_t = std::chrono::steady_clock::now();
-            tzero += std::chrono::duration_cast<std::chrono::nanoseconds>(
-                       end_t - start_t)
-                       .count();
-
-            start_t = std::chrono::steady_clock::now();
-            n = data[ss - 1];
-            end_t = std::chrono::steady_clock::now();
-            tnext += std::chrono::duration_cast<std::chrono::nanoseconds>(
-                       end_t - start_t)
-                       .count();
-            delete [] data;
-            output_percentage(i * 100 / iterations);
-        }
-        tzero = tzero / iterations;
-        tnext = tnext / iterations;
-        std::cout << static_cast<int64_t>(tzero) << " and " << static_cast<int64_t>(tnext)<< std::endl;
-    }
-    
-    do{
-        std::cout << "Do u want to look at it yourself? (0 to skip)" << std::endl;
+    size_t big_data_size = 1048576 * 10;
+    uint64_t* big_data = new uint64_t[big_data_size];
+    do {
+        std::cout << "Do u want to look at streak size? (0 to skip)"
+                  << std::endl;
         std::cin >> ss_end_val;
         tzero = 0;
         tnext = 0;
+        tdelta = 0;
+
+        auto start_t = std::chrono::steady_clock::now();
+        auto end_t = std::chrono::steady_clock::now();
+
+        for(size_t i = 0; i < iterations * iterations; ++i) {
+            start_t = std::chrono::steady_clock::now();
+            end_t = std::chrono::steady_clock::now();
+            tdelta += std::chrono::duration_cast<std::chrono::nanoseconds>(
+                        end_t - start_t)
+                        .count();
+        }
+        tdelta = tdelta / iterations / iterations;
+
         std::cout << "[     ] Checking " << ss_end_val << std::endl;
         for(size_t i = 0; i < iterations; ++i) {
             char* data = new char[ss_end_val];
 
-            auto start_t = std::chrono::steady_clock::now();
             data[0] = 100;
+            start_t = std::chrono::steady_clock::now();
             n = data[0];
-            auto end_t = std::chrono::steady_clock::now();
+            end_t = std::chrono::steady_clock::now();
             tzero += std::chrono::duration_cast<std::chrono::nanoseconds>(
                        end_t - start_t)
                        .count();
 
+            for(size_t j = 0; j < ss_end_val; ++j)
+                big_data[j] = j;
+
+            data[0] = 10;
             start_t = std::chrono::steady_clock::now();
-            data[0] = 101;
             n = data[ss_end_val - 1];
             end_t = std::chrono::steady_clock::now();
             tnext += std::chrono::duration_cast<std::chrono::nanoseconds>(
                        end_t - start_t)
                        .count();
-            delete [] data;
+            delete[] data;
             output_percentage(i * 100 / iterations);
         }
         tzero = tzero / iterations;
         tnext = tnext / iterations;
-        std::cout << static_cast<int64_t>(tzero) << " and " << static_cast<int64_t>(tnext)<< std::endl;
+        std::cout << tzero << " and " << tnext << std::endl;
     } while(ss_end_val);
-    
-    bool check_cache_latency = false;
+    delete[] big_data;
+
     std::cout << "Should I test latency? (0 to skip)" << std::endl;
-    std::cin >> check_cache_latency;
-    
-    if (check_cache_latency)
-    for(uint64_t ss = 8;ss < 128; ss*=2) {
-        //    guess_cache_streak_size();
-        //    std::cout << "Cache streak size is " << static_cast<int>(ss) <<
-        //    std::endl
-        //              << std::endl;
+    std::cin >> ss_end_val;
 
-        std::cout << "[     ] Measuring fwd latency" << std::endl;
-        for(size_t i = 1; i < 196608; i += 512) {
-            str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-            output_percentage((i / 196608.) * 100); /// 3.);
-        }
-        std::cout << std::endl;
-        for(size_t i = 196608; i < 393216; i += 8192) {
-            str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-            output_percentage((i / 393216.) * 100); /// 3.);
-        }
-        std::cout << std::endl;
-        for(size_t i = 393216; i < 1048576; i += 32768) {
-            str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-            output_percentage((i / 1048576.) * 100);
-        }
-        for(size_t i = 1048576; i < 16777216; i += 524288) {
-            str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-            output_percentage((i / 16777216.) * 100);
-        }
+    if(ss_end_val)
+        for(uint64_t ss = 8; ss < ss_end_val; ss *= 2) {
 
-        output_percentage(100);
+            std::cout << "[     ] Measuring fwd latency" << std::endl;
+            for(size_t i = 1; i < 196608; i += 512) {
+                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+                output_percentage((i / 196608.) * 100); /// 3.);
+            }
+            std::cout << std::endl;
+            for(size_t i = 196608; i < 393216; i += 8192) {
+                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+                output_percentage((i / 393216.) * 100); /// 3.);
+            }
+            std::cout << std::endl;
+            for(size_t i = 393216; i < 1048576; i += 32768) {
+                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+                output_percentage((i / 1048576.) * 100);
+            }
+            for(size_t i = 1048576; i < 16777216; i += 524288) {
+                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+                output_percentage((i / 16777216.) * 100);
+            }
 
-        std::ofstream out("results" + std::to_string(ss) + ".txt");
-        out /*<< i << " bytes is " */ << str;
-        str.clear();
-        //<< " ns"
-        //<< std::endl;
-        out.close();
-    }
+            output_percentage(100);
+
+            std::ofstream out("results" + std::to_string(ss) + ".txt");
+            out /*<< i << " bytes is " */ << str;
+            str.clear();
+            //<< " ns"
+            //<< std::endl;
+            out.close();
+        }
     return 0;
 }
--- cache_lab.cpp
+++ cache_lab.cpp
@@ -14,6 +14,7 @@ get_fwd_average_latency(const size_t data_size,
                         const size_t iterations = 1000)
 {
     /*
+     * Get forward run avg latency
      * data_size in bytes
      * average_latency in ns
      */
@@ -40,18 +41,164 @@ get_fwd_average_latency(const size_t data_size,
     return average_latency;
 }
 
+size_t
+get_rvs_average_latency(const size_t data_size,
+                        uint8_t streak_size,
+                        const size_t iterations = 1000)
+{
+    /*
+     * Get reverse run avg latency
+     * data_size in bytes
+     * average_latency in ns
+     */
+    size_t average_latency = 0;
+
+    char n;
+    char* data = new char[data_size];
+    char* data1 = new char[data_size];
+    for(size_t j = 0; j < data_size; j += streak_size)
+        data[j] = rand();
+    auto start_t = std::chrono::steady_clock::now();
+    for(size_t i = 0; i < iterations; ++i) {
+        for(size_t j = 0; j < data_size; j += streak_size) {
+            data1[data_size - j - 1] = data[data_size - j - 1];
+        }
+    }
+    auto end_t = std::chrono::steady_clock::now();
+    delete[] data;
+    delete[] data1;
+    average_latency +=
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t)
+        .count() /
+      iterations;
+    return average_latency;
+}
+
+size_t
+get_rnd_average_latency(const size_t data_size,
+                        uint8_t streak_size,
+                        const size_t iterations = 1000)
+{
+    /* Get random run avg latency
+     * data_size in bytes
+     * average_latency in ns
+     */
+    size_t average_latency = 0;
+
+    char n;
+    char* data = new char[data_size];
+    char* data1 = new char[data_size];
+    for(size_t j = 0; j < data_size; j += streak_size)
+        data[j] = rand();
+
+    auto start_t = std::chrono::steady_clock::now();
+    for(size_t i = 0; i < iterations; ++i) {
+        for(size_t j = 0; j < data_size; j += streak_size)
+            data1[rand() % data_size] = data[rand() % data_size];
+    }
+    auto end_t = std::chrono::steady_clock::now();
+    delete[] data;
+    delete[] data1;
+    average_latency +=
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t)
+        .count() /
+      iterations;
+    return average_latency;
+}
+
 void
 output_percentage(const short int score)
 {
     std::cout << "\033[1A[" << std::setw(3) << score << "% ]" << std::endl;
 }
 
+void measure_fwd_latency(size_t ss)
+{
+    std::string str;
+    std::cout << "[     ] Measuring forward run latency" << std::endl;
+    for(size_t i = 1; i < 196608; i += 512) {
+        str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 196608.) * 100); /// 3.);
+    }
+    for(size_t i = 196608; i < 393216; i += 8192) {
+        str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 393216.) * 100); /// 3.);
+    }
+    for(size_t i = 393216; i < 1048576; i += 32768) {
+        str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 1048576.) * 100);
+    }
+    for(size_t i = 1048576; i < 16777216; i += 524288) {
+        str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 16777216.) * 100);
+    }
+    output_percentage(100);
+    std::ofstream fwdout("fwd_results" + std::to_string(ss) + ".txt");
+    fwdout << str;
+    fwdout.close();
+}
+
+void
+measure_rvs_latency(size_t ss)
+{
+    std::string str;
+    std::cout << "[     ] Measuring reverse run latency" << std::endl;
+    for(size_t i = 1; i < 196608; i += 512) {
+        str += std::to_string(get_rvs_average_latency(i, ss)) + "\n";
+        output_percentage((i / 196608.) * 100); /// 3.);
+    }
+    for(size_t i = 196608; i < 393216; i += 8192) {
+        str += std::to_string(get_rvs_average_latency(i, ss)) + "\n";
+        output_percentage((i / 393216.) * 100); /// 3.);
+    }
+    for(size_t i = 393216; i < 1048576; i += 32768) {
+        str += std::to_string(get_rvs_average_latency(i, ss)) + "\n";
+        output_percentage((i / 1048576.) * 100);
+    }
+    for(size_t i = 1048576; i < 16777216; i += 524288) {
+        str += std::to_string(get_rvs_average_latency(i, ss)) + "\n";
+        output_percentage((i / 16777216.) * 100);
+    }
+    output_percentage(100);
+
+    std::ofstream rvsout("rvs_results" + std::to_string(ss) + ".txt");
+    rvsout << str;
+    rvsout.close();
+}
+
+void
+measure_rnd_latency(size_t ss)
+{
+    std::string str;
+    std::cout << "[     ] Measuring random latency" << std::endl;
+    for(size_t i = 1; i < 196608; i += 512) {
+        str += std::to_string(get_rnd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 196608.) * 100); /// 3.);
+    }
+    for(size_t i = 196608; i < 393216; i += 8192) {
+        str += std::to_string(get_rnd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 393216.) * 100); /// 3.);
+    }
+    for(size_t i = 393216; i < 1048576; i += 32768) {
+        str += std::to_string(get_rnd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 1048576.) * 100);
+    }
+    for(size_t i = 1048576; i < 16777216; i += 524288) {
+        str += std::to_string(get_rnd_average_latency(i, ss)) + "\n";
+        output_percentage((i / 16777216.) * 100);
+    }
+    output_percentage(100);
+    std::ofstream rndout("rnd_results" + std::to_string(ss) + ".txt");
+    rndout << str;
+    rndout.close();
+}
+
 int
 main()
 {
-    std::string str;
     size_t ss_beg_val = 0;
     size_t ss_end_val = 0;
+    bool answer = 0;
 
     std::cout << "Enter begin value (0 to skip)" << std::endl;
     std::cin >> ss_beg_val;
@@ -61,35 +208,20 @@ main()
 
     if(ss_end_val)
         for(uint64_t ss = ss_beg_val; ss <= ss_end_val; ss *= 2) {
-
-            std::cout << "[     ] Measuring fwd latency" << std::endl;
-            for(size_t i = 1; i < 196608; i += 512) {
-                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-                output_percentage((i / 196608.) * 100); /// 3.);
-            }
-            std::cout << std::endl;
-            for(size_t i = 196608; i < 393216; i += 8192) {
-                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-                output_percentage((i / 393216.) * 100); /// 3.);
-            }
-            std::cout << std::endl;
-            for(size_t i = 393216; i < 1048576; i += 32768) {
-                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-                output_percentage((i / 1048576.) * 100);
-            }
-            for(size_t i = 1048576; i < 16777216; i += 524288) {
-                str += std::to_string(get_fwd_average_latency(i, ss)) + "\n";
-                output_percentage((i / 16777216.) * 100);
-            }
-
-            output_percentage(100);
-
-            std::ofstream out("results" + std::to_string(ss) + ".txt");
-            out /*<< i << " bytes is " */ << str;
-            str.clear();
-            //<< " ns"
-            //<< std::endl;
-            out.close();
+            std::cout << "Should I measure fwd? (0 for not)" << std::endl;
+            std::cin >> answer;
+            if(answer)
+                measure_fwd_latency(ss);
+            std::cout << "Should I measure rvs? (0 for not)" << std::endl;
+            std::cin >> answer;
+            if(answer)
+                measure_rvs_latency(ss);
+            std::cout << "Should I measure rnd? (0 for not)" << std::endl;
+            std::cin >> answer;
+            if(answer)
+                measure_rnd_latency(ss);
+            std::cout << "Results for " << ss << " are ready, check *_results"
+                      << ss << "\\.txt" << std::endl;
         }
     return 0;
 }
