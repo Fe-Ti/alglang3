53f530d45d0eca2ae661468f9f7000ede125b663
Fe-Ti <btm.007@mail.ru>
2021-12-08 23:50:20 +0300

    Noving forward with lab03

--- sharedptr.cpp
+++ sharedptr.cpp
@@ -1,41 +1,90 @@
+//  Copyright 2021 Fe-Ti
+//  Shared Pointer
 #include <atomic>
 
 class SharedPtrControlBlock;
 
 template<typename T> class SharedPtr
 {
-    T* _cb_ptr;
+    SharedPtrControlBlock* _cb_ptr;
+    bool is_invalid() const
+    {
+        return _cb_ptr == nullptr;
+    }
 
   public:
     SharedPtr()
     {
-        _cb_ptr = SharedPtrControlBlock();
+        _cb_ptr = new SharedPtrControlBlock();
     }
     SharedPtr(T* ptr)
     {
-        _cb_ptr = SharedPtrControlBlock();
+        _cb_ptr = new SharedPtrControlBlock(ptr);
     }
-    SharedPtr(const SharedPtr& r);
-    SharedPtr(SharedPtr&& r);
-    ~SharedPtr();
-    auto operator=(const SharedPtr& r) -> SharedPtr&;
-    auto operator=(SharedPtr&& r) -> SharedPtr&;
+    SharedPtr(const SharedPtr& r)
+    {
+        _cb_ptr = r._cb_ptr;
+        if(!is_invalid())
+            _cb_ptr->inc_ref_cnt();
+    }
+    SharedPtr(SharedPtr&& r)
+    {
+        _cb_ptr = r._cb_ptr;
+        r._cb_ptr = nullptr;
+    }
+    ~SharedPtr()
+    {
+        if(is_invalid()) // if is invalid, i.e. _cb_prt == nullptr
+            return;      // do nothing
+        _cb_ptr->dec_ref_cnt();
+        if(!_cb_ptr->is_alive()) { // if is not alive
+            delete _cb_ptr;        // then delete the control block
+        }
+    }
 
-    // проверяет, указывает ли указатель на объект
+    SharedPtr& operator=(const SharedPtr& r)
+    {
+        if(!is_invalid())
+            _cb_ptr->dec_ref_cnt();
+        _cb_ptr = r._cb_ptr;
+        if(!is_invalid())
+            _cb_ptr->inc_ref_cnt();
+        return *this;
+    }
+    SharedPtr& operator=(SharedPtr&& r)
+    {
+        _cb_ptr = r._cb_ptr;
+        r._cb_ptr = nullptr;
+        return *this;
+    }
+
     operator bool() const;
     auto operator*() const -> T&;
     auto operator->() const -> T*;
 
-    auto get() -> T*;
-    void reset();
-    void reset(T* ptr);
-    void swap(SharedPtr& r);
-    // возвращает количество объектов SharedPtr, которые ссылаются на тот же
-    // управляемый объект
-    auto use_count() const -> size_t;
-    ~SharedPtr()
+    T* get() const
     {
-        
+        return _cb_ptr->get_raw_ptr();
+    }
+
+    void reset()
+    {
+        delete _cb_ptr->get_raw_ptr();
+    }
+    void reset(T* ptr)
+    {
+        ;
+    }
+
+    void swap(SharedPtr& r)
+    {
+        T* tmp = r._cb_ptr->get_raw_ptr();
+        r._cb_ptr->set_raw_ptr(this->_cb_ptr->get_raw_ptr());
+        this->_cb_ptr->set_raw_ptr(tmp);
+    }
+    size_t use_count() const
+    {
+        return _cb_ptr->use_count();
     }
 };
 
@@ -49,6 +98,10 @@ template<typename T> class SharedPtrControlBlock
     {
         this->_obj_ptr = _obj_ptr;
     }
+    ~SharedPtrControlBlock()
+    {
+        delete _obj_ptr;
+    }
 
     void inc_ref_cnt()
     {
@@ -57,9 +110,11 @@ template<typename T> class SharedPtrControlBlock
     void dec_ref_cnt()
     {
         --ref_cnt;
+        if(!is_alive())
+            ~SharedPtrControlBlock();
     }
 
-    T* get_raw_ptr()
+    T* get_raw_ptr() const
     {
         return _obj_ptr;
     }
@@ -68,11 +123,11 @@ template<typename T> class SharedPtrControlBlock
         this->_obj_ptr = _obj_ptr;
     }
 
-    size_t use_count()
+    size_t use_count() const
     {
         return ref_cnt;
     }
-    bool is_alive()
+    bool is_alive() const
     {
         return ref_cnt > 0;
     }
